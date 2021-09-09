aa778f79bcbc5afa29899c66c56cb054535535ee
busybox <planetgeek11@gmail.com>
2021-09-09 15:19:07 +0200

    app: Added responsive navbar

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/app.html'));
+})
+
 app.listen(port, () => {
   console.log(`Groupify web app listening on http://localhost:${port}`)
 })
